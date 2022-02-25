f148c1b35ddd454a25497061ef804ac2c2d049ba
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-02-25 21:04:38 +0700

    Update pingpong.cpp

--- pingpong.cpp
+++ pingpong.cpp
@@ -122,3 +122,96 @@ class cPaddle{
 		}
 
 };
+
+class cGameManger{
+	private:
+		int width, height;
+		int score1, score2;
+		char up1, down1, up2, down2;
+		bool quit;
+		cBall * ball;
+		cPaddle *player1;
+		cPaddle *player2;
+	public:
+		cGameManger(int w, int h){
+		
+			srand(time(NULL));
+			quit = false;
+			up1 = 'w'; up2 = 'i';
+			down1 = 's'; down2 = 'k';
+			score1 = score2 = 0;
+			width = w; height = h;
+			ball = new cBall(w / 2, h / 2);
+			player1	= new cPaddle(1, h / 2 - 3);
+			player2 = new cPaddle(w - 2, h / 2 - 3);
+		}
+		~cGameManger(){		
+			
+			delete ball, player1, player2;
+		}
+		void ScoreUp(cPaddle * player){
+			if (player == player1)
+				score1++;
+			else if (player == player2)
+				score2++;
+			
+			ball->Reset();
+			player1->Reset();
+			player2->Reset();
+		}
+		void Draw(){
+		
+			system("clear");
+			for(int i = 0; i < width + 2; i++)
+				cout << "#";
+			cout << endl;
+			
+			for(int i = 0; i < height; i++){
+				for(int j = 0; j < width; j++){
+					int ballx = ball->getX();
+					int bally = ball->getY();
+					int player1x = player1->getX();
+					int player2x = player2->getX();
+					int player1y = player1->getY();
+					int player2y = player2->getY();
+
+					if (j == 0)
+						cout << "#";
+
+					if (ballx == j && bally == i)
+						cout << "O"; // ball
+					else if (player1x == j && player1y == i)
+						cout << "|"; // player1
+					else if (player2x == j && player2y == i)
+						cout << "|"; // player2
+
+					else if (player1x == j && player1y + 1 == i)
+						cout << "|"; // player1
+					else if (player1x == j && player1y + 2 == i)
+						cout << "|"; // player1
+					else if (player1x == j && player1y + 3 == i)
+						cout << "|"; // player1
+
+					else if (player2x == j && player2y + 1 == i)
+						cout << "|"; // player2
+					else if (player2x == j && player2y + 2 == i)
+						cout << "|"; // player2
+					else if (player2x == j && player2y + 3 == i)
+						cout << "|"; // player2
+					else
+						cout << " ";
+
+					if (j == width - 1)
+						cout << "#";
+				}
+				cout << endl;
+			}
+
+			for(int i = 0; i < width + 2; i++)
+				cout << "#";
+			cout << endl;
+
+			cout << "Score 1 : " << score1 << endl;
+			cout << "Score 2 : " << score2 << endl;
+
+		}
