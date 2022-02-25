0edc0df851e67a18dbd781d8d1478ff0f144a896
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-02-25 21:01:42 +0700

    Update pingpong.cpp

--- pingpong.cpp
+++ pingpong.cpp
@@ -34,3 +34,30 @@ int _kbhit(void)
 
   return 0;
 }
+
+enum eDir {STOP = 0, LEFT = 1, UPLEFT = 2, DOWNLEFT = 3, RIGHT = 4, UPRIGHT = 5, DOWNRIGHT = 6};
+class cBall{
+	private:
+		int x,y;
+		int originalX, originalY;
+		eDir direction;
+	public:
+		cBall(int posX, int posY){
+			originalX = posX;
+			originalY = posY;
+			x = posX; y = posY;
+			direction = STOP;
+		}
+		void Reset(){
+			x = originalX; y = originalY;
+			direction = STOP;
+		}
+		void changeDirection(eDir d){
+			direction = d;
+		}
+		void randomDirection(){
+			direction = (eDir)((rand() % 6) + 1);
+		}
+		inline int getX() { return x; }
+		inline int getY() { return y; }
+		inline eDir getDirection() { return direction; }
